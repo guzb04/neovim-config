f0a14ce86fb32c7a584733a05a277e6c76e0460d
guzb04 <guzmanbeyhaut@gmail.com>
2024-08-02 02:07:28 -0300

    gruvbox

---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		name = "gruvbox-material",
+		priority = 1000,
+		config = function()
+        vim.cmd.colorscheme("gruvbox-material")
+    end,
+	},
+}
