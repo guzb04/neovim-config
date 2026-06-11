74bd51f0d18f37114b8b3b0f1ff331923f3b1625
Guzman Beyhaut <guzmanbeyhaut@gmail.com>
2026-06-11 20:59:45 -0300

    re added background to cmp

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -3,6 +3,8 @@ return {
   priority = 1000,
   config = function()
     vim.g.gruvbox_material_foreground = 'original'
+    vim.g.gruvbox_material_enable_bold = 1
+    vim.g.gruvbox_material_foreground = 'original'
     vim.cmd("colorscheme gruvbox-material")
   end
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
-{
-  "romus204/tree-sitter-manager.nvim",
-  dependencies = {}, -- tree-sitter CLI must be installed system-wide
-  config = function()
-    require("tree-sitter-manager").setup({
-       auto_install = true,
-       highlight = true
-    })
-  end
-}
+  {
+    "romus204/tree-sitter-manager.nvim",
+    dependencies = {}, -- tree-sitter CLI must be installed system-wide
+    config = function()
+      require("tree-sitter-manager").setup({
+        auto_install = true,
+        highlight = true,
+
+      })
+    end
+  }
 }
